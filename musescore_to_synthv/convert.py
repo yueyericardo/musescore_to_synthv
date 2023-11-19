6295e0eace6f1322e9e2014ab448526d79a8b2ba
yueyericardo <yueyericardo@gmail.com>
2023-11-18 23:45:17 -0500

    big refactor

--- musescore_to_synthv/convert.py
+++ musescore_to_synthv/convert.py
@@ -1,283 +1,7 @@
-import re
 import os
 import argparse
 from pathlib import Path
-from .common import jp_to_hr
 from . import musescore_parser
-
-ONE_BEAT = 705600000
-TAB = "  "
-
-time_signature_n = 0
-time_signature_d = 0
-pitch = 0
-duration = 0
-onset = 0
-lyric = ""
-last_lyric = ""
-comment = ""
-tie = False
-dot = 0
-staff_num = 0
-tuplet = False
-
-output_string = ""
-use_hr_dict = False
-
-
-def get_time_signature_duration(n, d):
-    # print("get_time_signature_duration")
-    if n == 4 and d == 4:
-        return int(ONE_BEAT * 4)
-    elif n == 3 and d == 4:
-        return int(ONE_BEAT * 3)
-    elif n == 6 and d == 8:
-        return int(6 * int(ONE_BEAT / 2))
-
-
-duration_type = {
-    "measure": None,
-    "whole": int(ONE_BEAT * 4),
-    "half": int(ONE_BEAT * 2),
-    "quarter": int(ONE_BEAT),
-    "eighth": int(ONE_BEAT / 2),
-    "16th": int(ONE_BEAT / 4),
-    "32nd": int(ONE_BEAT / 8),
-}
-
-
-def generate_lyric(l):
-    if l in ["-", "", "", None]:
-        return "-"
-    global use_hr_dict
-    if use_hr_dict:
-        string = "."
-        for letter in l:
-            try:
-                string += jp_to_hr.jp_to_hr[letter.lower()] + " "
-            except KeyError:
-                pass
-        return string[:-1]
-    return re.sub(r"\W+", "", l)
-
-
-def generate_project_start():
-    string = ""
-    string += "{" + "\n"
-    string += '    "version": 7,' + "\n"
-    string += '    "meter": [' + "\n"
-    string += "        {" + "\n"
-    string += '            "measure": 0,' + "\n"
-    string += '            "beatPerMeasure": 4,' + "\n"
-    string += '            "beatGranularity": 4' + "\n"
-    string += "        }" + "\n"
-    string += "    ]," + "\n"
-    string += '    "tempo": [' + "\n"
-    string += "        {" + "\n"
-    string += '            "position": 0,' + "\n"
-    string += '            "beatPerMinute": 90.0' + "\n"
-    string += "        }" + "\n"
-    string += "    ]," + "\n"
-    string += '    "tracks": [' + "\n"
-    return string
-
-
-def generate_staff_start():
-    global staff_num
-    string = ""
-    string += "        {" + "\n"
-    string += '            "name": "Unnamed Track",' + "\n"
-    string += '            "dbName": "Eleanor Forte",' + "\n"
-    string += '            "color": "15e879",' + "\n"
-    string += '            "displayOrder": ' + str(staff_num) + "," + "\n"
-    string += '            "dbDefaults": {},' + "\n"
-    string += '            "notes": [' + "\n"
-    return string
-
-
-def generate_note():
-    string = ""
-    string += "                {" + "\n"
-    string += '                    "onset": ' + str(onset) + "," + "\n"
-    string += '                    "duration": ' + str(duration) + "," + "\n"
-    string += '                    "lyric": "' + generate_lyric(lyric) + '",' + "\n"
-    string += '                    "comment": "' + str(lyric) + '",' + "\n"
-    string += '                    "pitch": ' + str(pitch) + "," + "\n"
-    string += '                    "dF0Vbr": 0.0' + "," + "\n"
-    string += '                    "dF0Jitter": 0.0' + "" + "\n"
-    string += "                }," + "\n"
-    return string
-
-
-def generate_staff_end():
-    string = ""
-    string += "            ]," + "\n"
-    string += '            "gsEvents": null,' + "\n"
-    string += '            "mixer": {' + "\n"
-    string += '                "gainDecibel": 0.0,' + "\n"
-    string += '                "pan": 0.0,' + "\n"
-    string += '                "muted": false,' + "\n"
-    string += '                "solo": false,' + "\n"
-    string += '                "engineOn": true,' + "\n"
-    string += '                "display": true' + "\n"
-    string += "            }," + "\n"
-    string += '            "parameters": {' + "\n"
-    string += '                "interval": 5512500,' + "\n"
-    string += '                "pitchDelta": [' + "\n"
-    string += "                    0," + "\n"
-    string += "                    0" + "\n"
-    string += "                ]," + "\n"
-    string += '                "vibratoEnv": [' + "\n"
-    string += "                    0," + "\n"
-    string += "                    0" + "\n"
-    string += "                ]," + "\n"
-    string += '                "loudness": [' + "\n"
-    string += "                    0," + "\n"
-    string += "                    0" + "\n"
-    string += "                ]," + "\n"
-    string += '                "tension": [' + "\n"
-    string += "                    0," + "\n"
-    string += "                    0" + "\n"
-    string += "                ]," + "\n"
-    string += '                "breathiness": [' + "\n"
-    string += "                    0," + "\n"
-    string += "                    0" + "\n"
-    string += "                ]," + "\n"
-    string += '                "voicing": [' + "\n"
-    string += "                    0," + "\n"
-    string += "                    0" + "\n"
-    string += "                ]," + "\n"
-    string += '                "gender": [' + "\n"
-    string += "                    0," + "\n"
-    string += "                    0" + "\n"
-    string += "                ]" + "\n"
-    string += "            }" + "\n"
-    string += "        }," + "\n"
-    return string
-
-
-def generate_project_end():
-    string = ""
-    string += "    ]," + "\n"
-    string += '    "instrumental": {' + "\n"
-    string += '        "filename": "",' + "\n"
-    string += '        "offset": 0.0' + "\n"
-    string += "    }," + "\n"
-    string += '    "mixer": {' + "\n"
-    string += '        "gainInstrumentalDecibel": 0.0,' + "\n"
-    string += '        "gainVocalMasterDecibel": 0.0,' + "\n"
-    string += '        "instrumentalMuted": false,' + "\n"
-    string += '        "vocalMasterMuted": false' + "\n"
-    string += "    }" + "\n"
-    string += "}" + "\n"
-    return string
-
-
-def set_staff_start():
-    global onset
-    onset = 0
-    # print(generate_staff_start())
-    global output_string
-    output_string += generate_staff_start()
-
-
-def set_staff_end():
-    # print(generate_staff_end())
-    global output_string
-    output_string += generate_staff_end()
-    global staff_num
-    staff_num += 1
-
-
-def set_time_signature(n, d):
-    global time_signature_n
-    time_signature_n = int(n)
-    global time_signature_d
-    time_signature_d = int(d)
-    global duration_type
-    duration_type["measure"] = get_time_signature_duration(
-        time_signature_n, time_signature_d
-    )
-
-
-def set_pitch(p, d):
-    global pitch
-    global duration
-    global output_string
-    global onset
-    global lyric
-    global tie
-    global dot
-
-    pitch = int(p)
-    duration += duration_type[d]
-
-    if dot > 0:
-        duration += int(duration_type[d] / 2)
-    if dot > 1:
-        duration += int(duration_type[d] / 4)
-    if dot > 2:
-        duration += int(duration_type[d] / 8)
-    dot = 0
-
-    if tuplet:
-        duration = duration * 2 / 3
-
-    if tie:
-        tie = False
-    else:
-        if lyric == "":
-            lyric = "-"
-        # print(generate_note())
-        output_string += generate_note()
-        onset += duration
-        duration = 0
-        lyric = ""
-
-
-def set_rest(d):
-    global onset
-    global dot
-
-    duration = duration_type[d]
-
-    if dot > 0:
-        duration += int(duration_type[d] / 2)
-    if dot > 1:
-        duration += int(duration_type[d] / 4)
-    if dot > 2:
-        duration += int(duration_type[d] / 8)
-    dot = 0
-
-    if tuplet:
-        duration = duration * 2 / 3
-
-    onset += duration
-
-
-def set_lyric(l):
-    global lyric
-    global last_lyric
-    lyric = l
-    last_lyric = l
-
-
-def set_tie():
-    global tie
-    tie = True
-
-
-def set_dot(num):
-    global dot
-    dot = int(num)
-
-
-def set_tuplet(status):
-    global tuplet
-    if status:
-        tuplet = True
-    else:
-        tuplet = False
 
 
 def write_to_file(file_name, data):
@@ -295,26 +19,12 @@ def prompt_overwrite(file_path):
     return response == "y"
 
 
-def convert(readfile, writefile, use_dict):
-    global output_string
-    global use_hr_dict
-    use_hr_dict = use_dict
-    output_string += generate_project_start()
-    musescore_parser.XMLParser().parse_xml(
-        readfile,
-        set_staff_start,
-        set_staff_end,
-        set_time_signature,
-        set_pitch,
-        set_rest,
-        set_lyric,
-        set_tie,
-        set_dot,
-        set_tuplet,
+def convert(readfile, writefile, use_hr_dict, verbose=False):
+    parser = musescore_parser.XMLParser(
+        readfile, verbose=verbose, use_hr_dict=use_hr_dict
     )
-    output_string += generate_staff_end()
-    output_string += generate_project_end()
-    write_to_file(writefile, output_string)
+    parser.parse_xml()
+    write_to_file(writefile, parser.output_string)
 
 
 def main():
@@ -324,6 +34,7 @@ def main():
         "writefile", type=str, nargs="?", help="Path to the output file"
     )
     parser.add_argument("-d", "--dict", action="store_true", help="Use dictionary flag")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose flag")
 
     args = parser.parse_args()
 
@@ -342,7 +53,7 @@ def main():
             print("Operation cancelled.")
             exit()
 
-    convert(args.readfile, args.writefile, args.dict)
+    convert(args.readfile, args.writefile, args.dict, args.verbose)
 
 
 if __name__ == "__main__":
--- musescore_to_synthv/musescore_parser.py
+++ musescore_to_synthv/musescore_parser.py
@@ -1,31 +1,330 @@
+import re
 import xml.etree.ElementTree as ET
-from .common import txt
+
+ONE_BEAT = 705600000
+
+
+class txt:
+    CEND = "\033[0m"
+    CBOLD = "\33[1m"
+    CITALIC = "\33[3m"
+    CURL = "\33[4m"
+    CBLINK = "\33[5m"
+    CBLINK2 = "\33[6m"
+    CSELECTED = "\33[7m"
+
+    CBLACK = "\33[30m"
+    CRED = "\33[31m"
+    CGREEN = "\33[32m"
+    CYELLOW = "\33[33m"
+    CBLUE = "\33[34m"
+    CVIOLET = "\33[35m"
+    CBEIGE = "\33[36m"
+    CWHITE = "\33[37m"
+
+    CBLACKBG = "\33[40m"
+    CREDBG = "\33[41m"
+    CGREENBG = "\33[42m"
+    CYELLOWBG = "\33[43m"
+    CBLUEBG = "\33[44m"
+    CVIOLETBG = "\33[45m"
+    CBEIGEBG = "\33[46m"
+    CWHITEBG = "\33[47m"
+
+    CGREY = "\33[90m"
+    CRED2 = "\33[91m"
+    CGREEN2 = "\33[92m"
+    CYELLOW2 = "\33[93m"
+    CBLUE2 = "\33[94m"
+    CVIOLET2 = "\33[95m"
+    CBEIGE2 = "\33[96m"
+    CWHITE2 = "\33[97m"
+
+    TAB = "  "
+
+
+jp_to_hr = {
+    "a": "aa",
+    "b": "b",
+    "c": "z",
+    "č": "ch",
+    "ć": "ch",
+    "d": "d",
+    "dž": "",
+    "đ": "jh",
+    "e": "eh",
+    "f": "f",
+    "g": "g",
+    "h": "hh",
+    "i": "iy",
+    "j": "y",
+    "k": "k",
+    "l": "l",
+    "lj": "",
+    "m": "m",
+    "n": "n",
+    "nj": "",
+    "o": "ow",
+    "p": "p",
+    "r": "r",
+    "s": "s",
+    "š": "sh",
+    "t": "t",
+    "u": "uw",
+    "v": "v",
+    "z": "z",
+    "ž": "zh",
+}
+
+
+def get_time_signature_duration(n, d):
+    # print("get_time_signature_duration")
+    if n == 4 and d == 4:
+        return int(ONE_BEAT * 4)
+    elif n == 3 and d == 4:
+        return int(ONE_BEAT * 3)
+    elif n == 6 and d == 8:
+        return int(6 * int(ONE_BEAT / 2))
 
 
 class XMLParser:
-    def __init__(self, verbose=False):
+    def __init__(self, readfile, verbose=False, use_hr_dict=False):
         self.verbose = verbose
-        self.set_staff_start_cb = None
-        self.set_staff_end_cb = None
-        self.set_time_signature_cb = None
-        self.set_pitch_cb = None
-        self.set_rest_cb = None
-        self.set_lyric_cb = None
-        self.set_tie_cb = None
-        self.set_dot_cb = None
-        self.set_tuplet_cb = None
+        self.xml_file = readfile
+        self.use_hr_dict = use_hr_dict
+
+        self.pitch = 0
+        self.duration = 0
+        self.onset = 0
+        self.lyric = ""
+        self.comment = ""
+        self.tie = False
+        self.dot = 0
+        self.staff_num = 0
+        self.tuplet = False
+        self.output_string = ""
+
         self.SET_TAB = txt.TAB * 30
 
-    def _print_verbose(self, *args):
-        if self.verbose:
-            print(*args)
+        self.duration_type = {
+            "measure": None,
+            "whole": int(ONE_BEAT * 4),
+            "half": int(ONE_BEAT * 2),
+            "quarter": int(ONE_BEAT),
+            "eighth": int(ONE_BEAT / 2),
+            "16th": int(ONE_BEAT / 4),
+            "32nd": int(ONE_BEAT / 8),
+        }
+
+    def generate_lyric(self, l):
+        if l in ["-", "", "", None]:
+            return "-"
+        if self.use_hr_dict:
+            string = "."
+            for letter in l:
+                try:
+                    string += jp_to_hr[letter.lower()] + " "
+                except KeyError:
+                    pass
+            return string[:-1]
+        return re.sub(r"\W+", "", l)
+
+    def generate_project_start(self):
+        string = ""
+        string += "{" + "\n"
+        string += '    "version": 7,' + "\n"
+        string += '    "meter": [' + "\n"
+        string += "        {" + "\n"
+        string += '            "measure": 0,' + "\n"
+        string += '            "beatPerMeasure": 4,' + "\n"
+        string += '            "beatGranularity": 4' + "\n"
+        string += "        }" + "\n"
+        string += "    ]," + "\n"
+        string += '    "tempo": [' + "\n"
+        string += "        {" + "\n"
+        string += '            "position": 0,' + "\n"
+        string += '            "beatPerMinute": 90.0' + "\n"
+        string += "        }" + "\n"
+        string += "    ]," + "\n"
+        string += '    "tracks": [' + "\n"
+        return string
+
+    def generate_staff_start(self):
+        string = ""
+        string += "        {" + "\n"
+        string += '            "name": "Unnamed Track",' + "\n"
+        string += '            "dbName": "Eleanor Forte",' + "\n"
+        string += '            "color": "15e879",' + "\n"
+        string += '            "displayOrder": ' + str(self.staff_num) + "," + "\n"
+        string += '            "dbDefaults": {},' + "\n"
+        string += '            "notes": [' + "\n"
+        return string
+
+    def generate_note(self):
+        string = ""
+        string += "                {" + "\n"
+        string += '                    "onset": ' + str(self.onset) + "," + "\n"
+        string += '                    "duration": ' + str(self.duration) + "," + "\n"
+        string += (
+            '                    "lyric": "'
+            + self.generate_lyric(self.lyric)
+            + '",'
+            + "\n"
+        )
+        string += '                    "comment": "' + str(self.lyric) + '",' + "\n"
+        string += '                    "pitch": ' + str(self.pitch) + "," + "\n"
+        string += '                    "dF0Vbr": 0.0' + "," + "\n"
+        string += '                    "dF0Jitter": 0.0' + "" + "\n"
+        string += "                }," + "\n"
+        return string
+
+    def generate_staff_end(self):
+        string = ""
+        string += "            ]," + "\n"
+        string += '            "gsEvents": null,' + "\n"
+        string += '            "mixer": {' + "\n"
+        string += '                "gainDecibel": 0.0,' + "\n"
+        string += '                "pan": 0.0,' + "\n"
+        string += '                "muted": false,' + "\n"
+        string += '                "solo": false,' + "\n"
+        string += '                "engineOn": true,' + "\n"
+        string += '                "display": true' + "\n"
+        string += "            }," + "\n"
+        string += '            "parameters": {' + "\n"
+        string += '                "interval": 5512500,' + "\n"
+        string += '                "pitchDelta": [' + "\n"
+        string += "                    0," + "\n"
+        string += "                    0" + "\n"
+        string += "                ]," + "\n"
+        string += '                "vibratoEnv": [' + "\n"
+        string += "                    0," + "\n"
+        string += "                    0" + "\n"
+        string += "                ]," + "\n"
+        string += '                "loudness": [' + "\n"
+        string += "                    0," + "\n"
+        string += "                    0" + "\n"
+        string += "                ]," + "\n"
+        string += '                "tension": [' + "\n"
+        string += "                    0," + "\n"
+        string += "                    0" + "\n"
+        string += "                ]," + "\n"
+        string += '                "breathiness": [' + "\n"
+        string += "                    0," + "\n"
+        string += "                    0" + "\n"
+        string += "                ]," + "\n"
+        string += '                "voicing": [' + "\n"
+        string += "                    0," + "\n"
+        string += "                    0" + "\n"
+        string += "                ]," + "\n"
+        string += '                "gender": [' + "\n"
+        string += "                    0," + "\n"
+        string += "                    0" + "\n"
+        string += "                ]" + "\n"
+        string += "            }" + "\n"
+        string += "        }," + "\n"
+        return string
+
+    def generate_project_end(self):
+        string = ""
+        string += "    ]," + "\n"
+        string += '    "instrumental": {' + "\n"
+        string += '        "filename": "",' + "\n"
+        string += '        "offset": 0.0' + "\n"
+        string += "    }," + "\n"
+        string += '    "mixer": {' + "\n"
+        string += '        "gainInstrumentalDecibel": 0.0,' + "\n"
+        string += '        "gainVocalMasterDecibel": 0.0,' + "\n"
+        string += '        "instrumentalMuted": false,' + "\n"
+        string += '        "vocalMasterMuted": false' + "\n"
+        string += "    }" + "\n"
+        string += "}" + "\n"
+        return string
+
+    def set_staff_start(self):
+        self.onset = 0
+        self.output_string += self.generate_staff_start()
+
+    def set_staff_end(self):
+        self.output_string += self.generate_staff_end()
+        self.staff_num += 1
 
     def set_time_signature(self, n, d):
         self._print_verbose(
             self.SET_TAB + txt.CRED + "set_time_signature" + txt.CEND, n, d
         )
-        if self.set_time_signature_cb:
-            self.set_time_signature_cb(n, d)
+        time_signature_n = int(n)
+        time_signature_d = int(d)
+        self.duration_type["measure"] = get_time_signature_duration(
+            time_signature_n, time_signature_d
+        )
+
+    def set_pitch(self, p, d):
+        self._print_verbose(self.SET_TAB + txt.CRED + "set_pitch" + txt.CEND, p, d)
+
+        self.pitch = int(p)
+        self.duration += self.duration_type[d]
+
+        if self.dot > 0:
+            self.duration += int(self.duration_type[d] / 2)
+        if self.dot > 1:
+            self.duration += int(self.duration_type[d] / 4)
+        if self.dot > 2:
+            self.duration += int(self.duration_type[d] / 8)
+        self.dot = 0
+
+        if self.tuplet:
+            self.duration = self.duration * 2 / 3
+
+        if self.tie:
+            self.tie = False
+        else:
+            if self.lyric == "":
+                self.lyric = "-"
+            # print(generate_note())
+            self.output_string += self.generate_note()
+            self.onset += self.duration
+            self.duration = 0
+            self.lyric = ""
+
+    def set_rest(self, d):
+        self._print_verbose(self.SET_TAB + txt.CRED + "set_rest" + txt.CEND, d)
+        duration = self.duration_type[d]
+
+        if self.dot > 0:
+            duration += int(self.duration_type[d] / 2)
+        if self.dot > 1:
+            duration += int(self.duration_type[d] / 4)
+        if self.dot > 2:
+            duration += int(self.duration_type[d] / 8)
+        self.dot = 0
+
+        if self.tuplet:
+            duration = duration * 2 / 3
+
+        self.onset += duration
+
+    def set_lyric(self, l):
+        self._print_verbose(self.SET_TAB + txt.CRED + "set_lyric" + txt.CEND, l)
+        self.lyric = l
+
+    def set_tie(self):
+        self._print_verbose(self.SET_TAB + txt.CRED + "set_tie" + txt.CEND)
+        self.tie = True
+
+    def set_dot(self, num):
+        self._print_verbose(self.SET_TAB + txt.CRED + "set_dot" + txt.CEND, num)
+        self.dot = int(num)
+
+    def set_tuplet(self, status):
+        self._print_verbose(self.SET_TAB + txt.CRED + "set_tuplet" + txt.CEND, status)
+        if status:
+            self.tuplet = True
+        else:
+            self.tuplet = False
+
+    def _print_verbose(self, *args):
+        if self.verbose:
+            print(*args)
 
     def parse_time_signature(self, v):
         n = d = ""
@@ -40,28 +339,6 @@ class XMLParser:
             elif t.tag == "sigD":
                 d = t.text
         self.set_time_signature(n, d)
-
-    def set_pitch(self, pitch, duration):
-        self._print_verbose(
-            self.SET_TAB + txt.CRED + "set_pitch" + txt.CEND, pitch, duration
-        )
-        if self.set_pitch_cb:
-            self.set_pitch_cb(pitch, duration)
-
-    def set_lyric(self, lyric):
-        self._print_verbose(self.SET_TAB + txt.CRED + "set_lyric" + txt.CEND, lyric)
-        if self.set_lyric_cb:
-            self.set_lyric_cb(lyric)
-
-    def set_tie(self):
-        self._print_verbose(self.SET_TAB + txt.CRED + "set_tie" + txt.CEND)
-        if self.set_tie_cb:
-            self.set_tie_cb()
-
-    def set_dot(self, num):
-        self._print_verbose(self.SET_TAB + txt.CRED + "set_dot" + txt.CEND, num)
-        if self.set_dot_cb:
-            self.set_dot_cb(num)
 
     def parse_chord(self, v):
         duration = ""
@@ -103,11 +380,6 @@ class XMLParser:
                     elif n.tag == "pitch":
                         self.set_pitch(n.text, duration)
 
-    def set_rest(self, duration):
-        self._print_verbose(self.SET_TAB + txt.CRED + "set_rest" + txt.CEND, duration)
-        if self.set_rest_cb:
-            self.set_rest_cb(duration)
-
     def parse_rest(self, v):
         for r in v.findall("./"):
             self._print_verbose(
@@ -118,11 +390,6 @@ class XMLParser:
             if r.tag == "durationType":
                 self.set_rest(r.text)
 
-    def set_tuplet(self, status):
-        self._print_verbose(self.SET_TAB + txt.CRED + "set_tuplet" + txt.CEND, status)
-        if self.set_tuplet_cb:
-            self.set_tuplet_cb(status)
-
     def parse_root(self, r):
         for s in r.findall("./Score/Staff"):
             self._print_verbose(
@@ -130,8 +397,7 @@ class XMLParser:
                 [s.attrib, s.tail.strip(), str(s.text).strip()],
             )
             m_count = 0
-            if self.set_staff_start_cb:
-                self.set_staff_start_cb()
+            self.set_staff_start()
             for m in s.findall("./"):
                 self._print_verbose(
                     txt.TAB * 1,
@@ -165,33 +431,12 @@ class XMLParser:
                                 elif v.tag == "endTuplet":
                                     self.set_tuplet(False)
 
-            if self.set_staff_end_cb:
-                self.set_staff_end_cb()
+            self.set_staff_end()
 
-    def parse_xml(
-        self,
-        xml_file,
-        set_staff_start_func=None,
-        set_staff_end_func=None,
-        set_time_signature_func=None,
-        set_pitch_func=None,
-        set_rest_func=None,
-        set_lyric_func=None,
-        set_tie_func=None,
-        set_dot_func=None,
-        set_tuplet_func=None,
-    ):
-        self.set_staff_start_cb = set_staff_start_func
-        self.set_staff_end_cb = set_staff_end_func
-        self.set_time_signature_cb = set_time_signature_func
-        self.set_pitch_cb = set_pitch_func
-        self.set_rest_cb = set_rest_func
-        self.set_lyric_cb = set_lyric_func
-        self.set_tie_cb = set_tie_func
-        self.set_dot_cb = set_dot_func
-        self.set_tuplet_cb = set_tuplet_func
-
-        tree = ET.parse(xml_file)
+    def parse_xml(self):
+        tree = ET.parse(self.xml_file)
         root = tree.getroot()
+        self.output_string += self.generate_project_start()
         self.parse_root(root)
+        self.output_string += self.generate_project_end()
         return root
